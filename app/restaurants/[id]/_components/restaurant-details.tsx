cdd3ccfd1132962d6aac1dbbc91559f09c2e49f8
1Edum <eduardomatosdesouza2@gmail.com>
2024-05-10 19:21:06 -0300

    test: erro list products

--- app/restaurants/[id]/_components/restaurant-details.tsx
+++ app/restaurants/[id]/_components/restaurant-details.tsx
@@ -1,23 +1,20 @@
-"use client";
-
 import DeliveryInfo from "@/app/_components/delivery-info";
-import { db } from "@/app/_lib/prisma";
-import { Category, Prisma, Restaurant } from "@prisma/client";
-
+import ProductList from "@/app/_components/product-list";
+import { Category, Product, Restaurant } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
 interface RestaurantDetailsProps {
-  restaurant: Pick<
-    Restaurant,
-    "name" | "imageUrl" | "deliveryFee" | "deliveryTimeMinutes"
-  > & {
-    categories: Category[]; // Supondo que 'categories' é uma matriz de strings
+  restaurant: Pick<Restaurant, "name" | "imageUrl" | "deliveryFee" | "deliveryTimeMinutes"> & {
+    categories: Category[];
   };
+  products: (Pick<Product, "id" | "name" | "description" | "imageUrl" | "price" | "discountPercentage" | "restaurantId" > & {
+    restaurants: Pick<Restaurant, "name">;
+  })[];
 }
 
-const RestaurantDetails = ({ restaurant }: RestaurantDetailsProps) => {
+const RestaurantDetails = ({ restaurant, products }: RestaurantDetailsProps) => {
   return (
     <div>
       <div className="rounded-t-3lg relative z-40 mt-[-1.5rem] rounded-3xl bg-white p-5">
@@ -33,7 +30,7 @@ const RestaurantDetails = ({ restaurant }: RestaurantDetailsProps) => {
             </div>
             <h1 className="text-xl font-semibold">{restaurant.name}</h1>
           </div>
-          <div className="flex items-center gap-[3px] rounded-full  bg-foreground px-2  py-[2px] text-white">
+          <div className="flex items-center gap-[3px] rounded-full bg-foreground px-2 py-[2px] text-white">
             <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
             <span className="text-xs font-semibold">5,0</span>
           </div>
@@ -45,13 +42,19 @@ const RestaurantDetails = ({ restaurant }: RestaurantDetailsProps) => {
           {restaurant.categories.map((category) => (
             <div
               key={category.id}
-              className="min-w-[167px] rounded-lg bg-[#f4f4f4] px-5 text-center [&::-webkit-scrollbar]:hidden mt-3"
+              className="mt-3 min-w-[167px] rounded-lg bg-[#f4f4f4] px-5 text-center [&::-webkit-scrollbar]:hidden"
             >
               <span className="text-xs text-muted-foreground">
                 {category.name}
               </span>
             </div>
           ))}
+        </div>
+
+        <div className="">
+          <h2 className="font-semibold">Mais Pedidos</h2>
+          {/* Passa a lista de produtos como prop para o componente ProductList */}
+          <ProductList products={} />
         </div>
       </div>
     </div>
